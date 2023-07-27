2d345ede2391ac1472068733cc40ab3dfbf54f74
Paulojoserc <pjrc83@gmail.com>
2023-07-26 23:41:06 -0300

    Anotações JPA para as classes de Domínio

--- src/main/java/br/com/pjtec/helpdesk/domain/Cliente.java
+++ src/main/java/br/com/pjtec/helpdesk/domain/Cliente.java
@@ -3,20 +3,29 @@ package br.com.pjtec.helpdesk.domain;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.persistence.Entity;
+import javax.persistence.OneToMany;
+
+import br.com.pjtec.helpdesk.domain.enums.Perfil;
+
+@Entity
 public class Cliente extends Pessoa {
-private List<Chamado> chamados = new ArrayList<>();
+	private static final long serialVersionUID = 1L;
+	@OneToMany(mappedBy = "cliente")
+	private List<Chamado> chamados = new ArrayList<>();
 
-public Cliente() {
-	super();
-}
+	public Cliente() {
+		super();
+		addPerfil(Perfil.CLIENTE);
+	}
 
-public Cliente(Integer id, String nome, String cpf, String email, String senha) {
-	super(id, nome, cpf, email, senha);
-}
+	public Cliente(Integer id, String nome, String cpf, String email, String senha) {
+		super(id, nome, cpf, email, senha);
+		addPerfil(Perfil.CLIENTE);
+	}
 
-public Cliente(List<Chamado> chamados) {
-	this.chamados = chamados;
-}
-
+	public Cliente(List<Chamado> chamados) {
+		this.chamados = chamados;
+	}
 
 }
