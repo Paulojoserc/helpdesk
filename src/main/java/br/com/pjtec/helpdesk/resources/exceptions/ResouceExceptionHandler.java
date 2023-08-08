ed9a28a5bc30c59023994258b3ad848fe76c58c4
Paulo Costa <pjrc83@gmail.com>
2023-08-08 17:45:01 -0300

    exceção para cpf e email criado

--- src/main/java/br/com/pjtec/helpdesk/resources/exceptions/ResouceExceptionHandler.java
+++ src/main/java/br/com/pjtec/helpdesk/resources/exceptions/ResouceExceptionHandler.java
@@ -7,6 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import br.com.pjtec.helpdesk.services.exceptions.DataIntegrityViolationException;
 import br.com.pjtec.helpdesk.services.exceptions.ObjectnotFoundException;
 
 @ControllerAdvice
@@ -17,5 +18,12 @@ public class ResouceExceptionHandler {
 		StandardError error = new StandardError(System.currentTimeMillis(), HttpStatus.NOT_FOUND.value(),"Object Not Found", ex.getMessage(), request.getRequestURI());
 		
 		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(error);
+	}
+	
+	@ExceptionHandler(DataIntegrityViolationException.class)
+	public ResponseEntity<StandardError> dataIntegrityViolationException(DataIntegrityViolationException ex, HttpServletRequest request){
+		StandardError error = new StandardError(System.currentTimeMillis(), HttpStatus.BAD_REQUEST.value(),"Violação de dados", ex.getMessage(), request.getRequestURI());
+		
+		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(error);
 	}
 }
