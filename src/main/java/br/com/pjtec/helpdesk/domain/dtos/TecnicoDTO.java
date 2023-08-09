502afcffd607157e18ece0b23519f4e200967d77
Paulo Costa <pjrc83@gmail.com>
2023-08-09 18:14:47 -0300

    Validacao nos campos de criação de novo usuário

--- src/main/java/br/com/pjtec/helpdesk/domain/dtos/TecnicoDTO.java
+++ src/main/java/br/com/pjtec/helpdesk/domain/dtos/TecnicoDTO.java
@@ -6,6 +6,8 @@ import java.util.HashSet;
 import java.util.Set;
 import java.util.stream.Collectors;
 
+import javax.validation.constraints.NotNull;
+
 import com.fasterxml.jackson.annotation.JsonFormat;
 
 import br.com.pjtec.helpdesk.domain.Tecnico;
@@ -14,10 +16,13 @@ import br.com.pjtec.helpdesk.domain.enums.Perfil;
 public class TecnicoDTO  implements Serializable {
 	private static final long serialVersionUID = 1L;
 	protected Integer id;
+	@NotNull(message = "O campo NOME é obrigatorio")
 	protected String nome;
-	
+	@NotNull(message = "O campo CPF é obrigatorio")
 	protected String cpf;
+	@NotNull(message = "O campo EMAIL é obrigatorio")
 	protected String email;
+	@NotNull(message = "O campo SENHA é obrigatorio")
 	protected String senha;
 	protected Set<Integer> perfis = new HashSet<>();
 	@JsonFormat(pattern = "dd/MM/yyyy")
