61f32ec188dadc4739faa6b90cba8ffbad0e2e70
Paulo Costa <pjrc83@gmail.com>
2023-10-02 09:26:01 -0300

    componente HOME criado

--- frontend/helpdesk/src/app/app.module.ts
+++ frontend/helpdesk/src/app/app.module.ts
@@ -28,10 +28,12 @@ import { MatListModule } from '@angular/material/list';
 import { MatCardModule } from '@angular/material/card';
 //Componentes do projeto
 import { NavComponent } from './components/nav/nav.component';
+import { HomeComponent } from './components/home/home.component';
 @NgModule({
   declarations: [
     AppComponent,
     NavComponent,
+    HomeComponent,
   
   ],
   imports: [
